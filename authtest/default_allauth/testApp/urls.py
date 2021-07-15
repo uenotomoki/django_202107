012f98f145b8d15c0bd6d62d9edb41a000b47c59
uenotomoki <gifucom17@gmail.com>
2021-07-15 21:24:14 +0900

    “ユニットテスト：実装一部完了”

--- authtest/default_allauth/testApp/urls.py
+++ authtest/default_allauth/testApp/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', TopView.as_view(), name='top'),
-    path('<int:num>', TopView.as_view(), name='top'),
+    path('<int:num>', TopView.as_view(), name='top_num'),
     path('snscreate/', SnsCreateView.as_view(), name='snscreate'),
     path('snsdelete/<int:num>/', SnsDeleteView.as_view(), name='snsdelete'),
     path('mysnsshow/', MySnsShowView.as_view(), name='mysnsshow'),
